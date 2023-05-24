ce9ed3d04c8a7e4059c180dfb1c497353e250aa6
Hristijan <chrispetrov904@gmail.com>
2023-05-24 12:41:31 +0200

    Added a TIE and a reset button that work

--- js.js
+++ js.js
@@ -2,8 +2,11 @@ const gameBoard = ( () => {
     let gameboard = Array(9);
     
     
-    const print = () =>{
-        console.log(gameboard);
+    const checkFull = () =>{
+        if(gameboard.includes(undefined)){
+            return false
+        }
+        else{return true}
     }//samo za testiranje
     
 
@@ -17,7 +20,32 @@ const gameBoard = ( () => {
         gameboard = Array(9);
     }
 
-    return {print , get , set , clearBoard}
+    return {print , get , set , clearBoard,checkFull}
+}
+)();
+
+const displayController = ( () => {
+    let gameDone=true
+    const disableClick = () =>{
+        if(!gameDone){
+            const divItems = document.getElementsByClassName("item")
+            
+            for (var i=0;i<9;i++){
+                divItems[i].removeEventListener('click',placeTile)
+                
+            }
+            
+            const divBoard = document.getElementsByTagName('p');
+            divBoard[0].innerText+="Game is Over"
+        }
+    }
+    function get(){
+        return gameDone
+    }
+    function set(state){
+        gameDone=state
+    }
+    return {get,set,disableClick}
 }
 )();
 
@@ -37,29 +65,30 @@ function populateBoard(board){
         const item= document.createElement('div')
         item.style.border = '1px solid blue'
         item.innerText= board[i]
+        item.className='item'
+        
         item.style.color='rgb(40, 28, 236)'
         item.addEventListener('click', placeTile.bind(null,i,item))
         divBoard.appendChild(item)
     }
     
 }
-// 1=X 2=0
+//start off by populating the board
 populateBoard(gameBoard);
-function placeTile(number,tile){
-    
-    
 
+// 2=X 3=0
+function placeTile(number,tile){   
     if(tile.innerText=='undefined'){
         if(newguy.status){
             newguy.status=false;
-            gameBoard.set(number,1);
+            gameBoard.set(number,2);
             
             tile.innerText='X';
         }
         else{
             //the oldguys turn
             newguy.status=true;
-            gameBoard.set(number,2)
+            gameBoard.set(number,3)
             
             tile.innerText='O';
         }
@@ -67,10 +96,50 @@ function placeTile(number,tile){
     else {console.log("This tile is already taken")}
     
     checkWon()
-    
+    if(displayController.get()){
+        if(gameBoard.checkFull()){
+            
+            displayController.set(false);
+            console.log("It's a TIE")
+            displayController.disableClick();
+        }
+    }
 }
 
 function checkWon(){
-    var gb=gameBoard.get()
+    const gb=gameBoard.get();
+    let state=displayController.get();
+    if(state){
+    if(((gb[0]===gb[1]&& gb[1]===gb[2]) && gb[1]!=undefined)||((gb[3]===gb[4]&& gb[3]===gb[5]) && gb[4]!=undefined)||((gb[6]===gb[7]&& gb[6]===gb[8]) && gb[7]!=undefined)){
+        console.log("WON horizontaly");
+        displayController.set(false);
+        displayController.disableClick();
+        
+    }
+    else if(((gb[0]===gb[3]&& gb[0]===gb[6]) && gb[3]!=undefined)||((gb[1]===gb[4]&& gb[4]===gb[7]) && gb[4]!=undefined)||((gb[2]===gb[5]&& gb[2]===gb[8]) && gb[5]!=undefined)){
+        console.log("won verticaly");
+        displayController.set(false);
+        displayController.disableClick();
+    }
+    else if(((gb[0]===gb[4]&& gb[4]===gb[8]) && gb[4]!=undefined)||((gb[2]===gb[4]&& gb[4]===gb[6]) && gb[4]!=undefined)){
+        console.log("Won diagonaly");
+        displayController.set(false);
+        displayController.disableClick();
+        //tell the DOM that the game is over
+
+    }
     
+  }
+    
+
+}
+
+function reset(){
+    newguy= Player('One', true)
+    oldguy = Player('Two', false)
+    gameBoard.clearBoard()
+    console.log(gameBoard.get())
+    const divBoard = document.getElementById('board')
+    divBoard.innerHTML=''
+    populateBoard(gameBoard);
 }
